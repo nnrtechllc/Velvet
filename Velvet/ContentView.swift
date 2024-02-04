0019c663ee5b924ef49e665a126d4db72689504d
nnrtechllc <nnrtechnologiesllc@gmail.com>
2024-02-04 16:05:04 -0500

    Initial Commit

---
+++ Velvet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Velvet
+//
+//  Created by Nikhil Vaddey on 2/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, worl!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Velvet/VelvetApp.swift
@@ -0,0 +1,19 @@
+//
+//  VelvetApp.swift
+//  Velvet
+//
+//  Created by Nikhil Vaddey on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct VelvetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+            //m 
+            ContentView()
+        }
+    }
+}
